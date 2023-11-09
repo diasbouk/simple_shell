6f1621aa736ba163643414a1748c9996b09900eb
diasbouk <boukdsaid@gmail.com>
2023-11-09 14:07:09 +0100

    TASK 0 1

--- shell2.c
+++ shell2.c
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #define BUFFER_SIZE 0
 #define BUFF_NUM 0
+#define DELIM " \t\n"
 
 /**
  * handle_command - command into pathname
@@ -31,12 +32,16 @@ void handle_command(char *cmd)
 void _free_t(char **arr)
 {
     int i = 0;
+    if (!arr)
+        return;
     while (arr[i])
     {
         free(arr[i]);
+        arr[i] = NULL;
         i++;
     }
-    
+    free(arr);   
+    arr = NULL; 
 }
 
 /**
@@ -50,15 +55,13 @@ char *get_line(void)
     char *buffer = NULL;
     size_t buffer_size;
     int num_of_chars;
-        if (isatty(STDIN_FILENO) == 1)
+        if (isatty(STDIN_FILENO))
         write(STDOUT_FILENO, "[yassine_dias@shell]: ~$ ", 26);
         num_of_chars = getline(&buffer, &buffer_size, stdin);
         if (num_of_chars == -1)
             {
-                if(isatty(STDIN_FILENO) == 1)
-                    write(STDOUT_FILENO, "\n", 2);
                 free(buffer);
-                return (NULL);
+                 return (NULL);
             }
         buffer[num_of_chars - 1] = '\0';
         return (buffer);
@@ -78,13 +81,24 @@ char ** cmd_split(char *str)
     char **command = NULL;
     int i = 0, count = 0;
     dup = strdup(str);
-    token = strtok(dup, " \t");
+    token = strtok(dup, DELIM);
     while (token)
     {
-        
+        count++;
+        token = strtok(NULL, DELIM);
     }
-    
-    
+    free(dup);
+    command = malloc(sizeof(char *) * count + 1);
+    token = strtok(str, DELIM);
+    while (token)
+    {
+        command[i] = token;
+        token = strtok(NULL, DELIM);
+        i++;
+    }
+    command[i] = NULL;
+    return(command);
+    free(str);
 }
 
 /**
@@ -95,24 +109,24 @@ char ** cmd_split(char *str)
 */
 int exec(char **command,const char **argv, char **envp)
 {
-    pid_t pid;
+    pid_t forked;
     int status;
-    pid = fork();
-    if (pid == 0)
+    forked = fork();
+    if (forked == 0)
     {
         if (execve(command[0], command, envp) == -1)
         {
             perror(argv[0]);
-            _free_t(command);
-            exit(status);
+             _free_t(command);
+             exit(0);
         }
     }
     else
     {
-        wait(&pid);
+        waitpid(forked, &status, 0);
         _free_t(command);
     }
-    return (status);
+    return (WEXITSTATUS(status));
 }
 
 /**
@@ -125,9 +139,7 @@ int main(int argc, char const **argv, char **envp)
 {
     char *argm;
     char **command;
-    int status = 0;
-    pid_t pid;
-    
+    int status = 0;    
         (void)argc;
         (void)argv;
 
@@ -135,9 +147,16 @@ int main(int argc, char const **argv, char **envp)
         {
             argm = get_line();
             if (argm == NULL)
+            {
+                if(isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 2);
                 return (status);
+                exit(WEXITSTATUS(status));
+            }
             command = cmd_split(argm);
-            exec(command, argv, envp);
+            if (command == NULL)
+                continue;
+            status = exec(command, argv, envp);
         }
             return 0;
 }
--- simple_shell.c
+++ simple_shell.c
@@ -10,42 +10,78 @@
 #include <fcntl.h>
 
 /**
- * cmd_split - check ...
- * 
+ * cmd_split - function to split command into separated strings
+ * @str: command to split
+ * Return: AN array of strings from command
 */
 
+char ** cmd_split(char *str)
+{
+	char *token = NULL;
+    char *dup = NULL;
+    char **command = NULL;
+    int i = 0, count = 0;
+    dup = strdup(str);
+    token = strtok(dup, " \t");
+    while (token)
+    {
+        count++;
+        token = strtok(NULL, " \t");
+    }
+    free(dup);
+    command = malloc(sizeof(char *) * count + 1);
+    token = strtok(str, " \t");
+    while (token)
+    {
+        command[i] = token;
+        token = strtok(NULL, " \t");
+        i++;
+    }
+    command[i] = NULL;
+    return(command);
+}
 
 /**
- * main - main function of dir
- * 
+ * get_line - get a command 
+ * Return: pointer to cmd if success
+ * -> NULL if not
 */
-int main()
+
+char *get_line(void)
 {
-	char *buffer = NULL; 
+    char *buffer;
 	size_t buffer_size;
-	char *commands;
-	char **command;
-	//	while (1)
-	//
-				int i = 0;
-				int child_pid = fork();
-				if (child_pid == 0)
-				{
-				 
-				}
-				else
-				{
-					wait(&child_pid);
-					while(i < 4)
-					{
-						puts("[Dias@Archiso]~$: ");
-						buffer_size = getline(&buffer,&buffer_size, stdin);
-						buffer[buffer_size - 1] = '\0';
-						commands = strtok(buffer, " ");
-						execve(commands, command, NULL);
-					}
-					printf("Error !\n");
-				}
-	//	}
+	size_t chars_num;
+	if (isatty(STDIN_FILENO) == 1)
+		write(STDOUT_FILENO, "[yassine_dias@shell]: ~$ ", 26);
+	chars_num = getline(&buffer, &buffer_size, stdin);
+	if (chars_num == -1)
+            {
+                if(isatty(STDIN_FILENO) == 1)
+                    write(STDOUT_FILENO, "\n", 2);
+                free(buffer);
+                return (NULL);
+            }
+        buffer[chars_num - 1] = '\0';
+        return (buffer);
+        free(buffer);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	char *buffer, **command;
+	int i = 0;
+	buffer = get_line();
+
+	if (buffer = NULL) /*if CTRL + D or EOF*/
+		exit(0);
+		printf("%s\n", buffer);
+	
+	command = cmd_split(buffer);
+	while (command[i])
+	{
+		printf("%s\n", command[i]);
+	}
+	
 	return (0);
 }
