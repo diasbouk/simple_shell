a3fb0660d21d49ba61a706559378d83bde1bfa95
diasbouk <boukdsaid@gmail.com>
2023-11-16 12:34:41 +0100

    TASK 0

--- command_split.c
+++ command_split.c
@@ -14,7 +14,7 @@ char ** cmd_split(char *str)
 	char *token;
     _strcpy(temp, str);
     token = strtok(temp, DELIM);
-    
+
     while (token != NULL)
     {
         count++;
@@ -31,10 +31,11 @@ char ** cmd_split(char *str)
         }
         splited[i] = NULL;
 
-		return(splited);
-        free(splited);
         free(str);
         str = NULL;
         free(temp);
         temp = NULL;
+
+		return(splited);
+        
 }
